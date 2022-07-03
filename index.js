9d3727ced1bf1f08bf973693cd08ed8c8f97cfe5
funnydev2512 <77485651+funnydev2512@users.noreply.github.com>
2022-07-03 10:01:19 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,79 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const { engine } = require('express-handlebars');
+const methodOverride = require('method-override')
+const app = express();
+const path = require('path');
+const port = 3000;
+const morgan = require('morgan');
 
-app.get('/tin-tuc', (req, res) => {
-  res.send('Hello World!')
-})
+
+const SortMiddleware = require('./app/middlewares/SortMiddleware');
+const route = require('./routes');
+const db = require('./config/db')
+//Connect to DB 
+db.connect();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+// app.engine("handlebars", handlebars());
+app.engine(
+    'hbs',
+    engine({
+        extname: 'hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field == sort.column ? sort.type : 'default';
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending'
+                }
+
+                const types ={
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc'
+                }
+
+                const icon = icons[sortType]
+                const type = types[sortType]
+
+                return `<a href="?_sort&column=${field}&type=${type}">
+                        <span class="${icon}"></span>
+                    </a>`
+
+                
+            }
+        },
+
+
+
+    }),
+
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
+app.use(express.json());
+
+app.use(methodOverride('_method'))
+
+app.use(SortMiddleware);
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+    console.log(`App listening on port http://localhost:${port}`);
+});
